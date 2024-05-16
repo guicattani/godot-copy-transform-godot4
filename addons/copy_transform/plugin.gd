a81322f07325ca6858fd270c2d40a08b2ea3f589
Guilherme Cattani <gcattani1@gmail.com>
2024-05-16 16:04:34 -0300

    Update plugin.gd

--- addons/copy_transform/plugin.gd
+++ addons/copy_transform/plugin.gd
@@ -1,36 +1,32 @@
-tool
+@tool
 extends EditorPlugin
 
-var copied_transform:Transform = Transform.IDENTITY
+var copied_transform:Transform3D = Transform3D.IDENTITY
 
-func _enter_tree():
-	add_tool_menu_item("Copy Transform", self, "copy_transform")
-	add_tool_menu_item("Paste Transform", self, "paste_transform")
+#Remember to add this script to Autoload or to an object
+func _ready():
+	add_tool_menu_item("Copy Transform", _copy_transform)
+	add_tool_menu_item("Paste Transform", _paste_transform)
 
-
-func _exit_tree():
-	remove_tool_menu_item("Copy Transform")
-	remove_tool_menu_item("Paste Transform")
-
-func copy_transform(thing):
+func _copy_transform():
 	var nodes = get_editor_interface().get_selection().get_selected_nodes()
-	if nodes.size() == 0: _show_message("Select a Spatial and try again.")
+	if nodes.size() == 0: _show_message("Select a Node3D and try again.")
 	elif nodes.size() > 1: _show_message("Too many nodes selected, only select one to copy.")
 	else:
 		var node = nodes[0]
-		if !node is Spatial: _show_message("Selection must be a Spatial.")
+		if !node is Node3D: _show_message("Selection must be a Node3D.")
 		else:
-			var sp = node as Spatial
+			var sp = node as Node3D
 			copied_transform = sp.transform
 			print("Copied transform: ", copied_transform)
 
-func paste_transform(thing):
+func _paste_transform():
 	var nodes = get_editor_interface().get_selection().get_selected_nodes()
-	if nodes.size() == 0: _show_message("Select a Spatial and try again.")
+	if nodes.size() == 0: _show_message("Select a Node3D and try again.")
 	else:
 		for node in nodes:
-			if node is Spatial:
-				var sp = node as Spatial
+			if node is Node3D:
+				var sp = node as Node3D
 				sp.transform = copied_transform
 				print("Pasted transform: ", copied_transform)
 
